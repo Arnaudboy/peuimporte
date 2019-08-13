96c6eebb5564e513ffeb01445fdbe45f36b652b2
Arnaudboy <r.boyer895@laposte.net>
2019-08-13 18:43:20 +0200

    ajout de la view event#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get 'registrations/new'
   end
   devise_for :users
-  resource :event
+  resources :event
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'event#index'
