47e22211000936c41608893affbcc359a144b234
Arnaudboy <r.boyer895@laposte.net>
2019-08-13 12:05:00 +0200

    Suppression de la migration user et creation de la devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
-	has_many :events, through: :participations
-	has_many :participations
-
-	after_create :welcome_send
-
-	def welcome_send
-		UserMailer.welcome_email(self).deliver_now
-	end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
