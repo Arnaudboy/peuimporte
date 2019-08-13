1a43bf3c2d4f59a3ba852f607ceb47655e1bf62e
Arnaudboy <r.boyer895@laposte.net>
2019-08-13 12:20:13 +0200

    modification du event#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events
+  resource :event
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to => 'events#index'
+  root :to => 'event#index'
 end
